57a5f04e1fd63ea717d1fca2ad98d07b591fd723
soloDev <com.solodev@gmail.com>
2026-08-07 06:56:18 +0000

    I added other types of while loo

--- Main.kt
+++ Main.kt
@@ -1,4 +1,6 @@
 fun main() {
+    //All my base java knowledge is applied in this code.
+
    val myName = "Reyny Markk X14";
    val myBday = "June 14, 2007";
    var myAge = 100;
@@ -14,39 +16,54 @@ fun main() {
        "No"
    }
    
-   var text = hasPortfolio + ", I have a portfolio";
-   text = "${hasPortfolio}, I have a portfolio"; //it works the same with above but I'm testing if this kind of syntax is possible
-    
+    var text = hasPortfolio + ", I have a portfolio";
+    text = "${hasPortfolio}, I have a portfolio"; //it works the same with above but I'm testing if this kind of syntax is possible
     println(text)
     
-    println("Pick a random number, let's see what i prepared for you: ")
-    var choice = readLine()!!.toInt()   //github uses outdated kotlin stability so i had to use this old syntax...i didnt use try chat and range validator
+    var choice = 0;  
+
+    while (choice !in 1..100){
+        println("Pick a random number between 1 and 100, let's see what i prepared for you: ")
+        choice = readLine()?.toIntOrNull() ?: 0
+    }
 
     when (choice) {
         1 -> println("Why 1? Out of many number, you chose 1")
         in 2..10 -> println("really? Out of many numbers, you chose this small value")
-        in 11..18 -> println("really? teenage?")
+        in 11..19 -> println("really? teenage?")
         else -> println("Thanks for trying my program!")
     }
-
-    print("")
+    println();
 
     for (i in 1..10){
         print("${i} ")
     }
-    println();
+    println("\n");
 
     var c = 0;
 
     while (c <= 5) {
-        print("This is a while loop ${c} ")
+        if (c == 3) {
+            break;
+        }
+        println("This is a while loop ${c} ")
         c++;
     }
     println();
 
     do {
-        println("This is a do-while Loop");
+        if (c == 3) {
+            c--;
+            continue;
+        }
+        println("This is a do-while Loop ${c}");
         c--;
-    } while (c >= 5);
+    } while (c >= 1);
     println();
+
+    val arrOfNum = intArrayOf(12, 13, 15, 20, 50)
+
+    for (i in arrOfNum.indices) {
+        println(arrOfNum[i])
+    }
 }
